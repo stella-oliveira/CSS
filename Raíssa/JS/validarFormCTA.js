86dd1ff04765eb36094455635f107208626c43e8
stella-oliveira <stella.1609@gmail.com>
2021-05-07 08:55:18 -0300

    Update Raíssa

---
+++ Raíssa/JS/validarFormCTA.js
@@ -0,0 +1,62 @@
+$.validator.setDefaults( {
+	submitHandler: function () {
+	//	alert( "Mensagem enviada!" );
+	}
+} );
+
+$( document ).ready( function () {
+
+	$( "#formularioContato" ).validate( {
+		rules: {
+			name: {
+            	required: true,
+            	minlength: 2
+          	},
+			email: {
+				required: true,
+				email: true
+			},
+		},
+		messages: {
+			name: {
+            	required: "O campo é obrigatório.",
+            	minlength: "O campo deve conter no mínimo 2 caracteres."
+          	},
+			email: "E-mail inválido.",
+		},
+		errorElement: "em",
+		errorPlacement: function ( error, element ) {
+			// Add the `help-block` class to the error element
+			error.addClass( "help-block" );
+
+			// Add `has-feedback` class to the parent div.form-group
+			// in order to add icons to inputs
+			element.parents( ".form-icon" ).addClass( "has-feedback" );
+
+			if ( element.prop( "type" ) === "checkbox" ) {
+				error.insertAfter( element.parent( "label" ) );
+			} else {
+				error.insertAfter( element );
+			}
+
+			// Add the span element, if doesn't exists, and apply the icon classes to it.
+			if ( !element.next( "span" )[ 0 ] ) {
+				$( "<span class='glyphicon glyphicon-remove form-control-feedback'></span>" ).insertAfter( element );
+			}
+		},
+		success: function ( label, element ) {
+			// Add the span element, if doesn't exists, and apply the icon classes to it.
+			if ( !$( element ).next( "span" )[ 0 ] ) {
+				$( "<span class='glyphicon glyphicon-ok form-control-feedback'></span>" ).insertAfter( $( element ) );
+			}
+		},
+		highlight: function ( element, errorClass, validClass ) {
+			$( element ).parents( ".form-icon" ).addClass( "has-error" ).removeClass( "has-success" );
+			$( element ).next( "span" ).addClass( "glyphicon-remove" ).removeClass( "glyphicon-ok" );
+		},
+		unhighlight: function ( element, errorClass, validClass ) {
+			$( element ).parents( ".form-icon" ).addClass( "has-success" ).removeClass( "has-error" );
+			$( element ).next( "span" ).addClass( "glyphicon-ok" ).removeClass( "glyphicon-remove" );
+		}
+	} );
+} );
